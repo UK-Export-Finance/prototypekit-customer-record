2d7dd07a8a3748ff70ef70304ff0e96b97c5a87e
ricky saunders <ricky.saunders@digital.hmrc.gov.uk>
2023-09-18 07:21:10 +0100

    test

--- app/assets/javascripts/modal-examples.js
+++
@@ -1,8 +0,0 @@
-let toggler = "input[type='radio'][name='partial-pay-attendance']"
-let target = $("input[type='radio'][name='partial-pay-radios']")
-
-$(document).on('change', toggler, function(e){
-	if($(toggler + ":checked").val() == 'yes'){
-		target.parent().parent().hide()
-	}
-})
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eu pipefail
-
-# node ./node_modules/gulp/bin/gulp generate-assets && node listen-on-port.js
-
-npm run build && npm run serve
